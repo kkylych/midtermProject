734aaf77633216b6b69eed67de44aff79ed4e153
kkylch <kchannel27@gmail.com>
2024-03-23 18:47:17 +0600

    add NoteDTO

---
+++ src/main/java/com/kylych/notesapp/dto/NoteMapper.java
@@ -0,0 +1,19 @@
+package com.kylych.notesapp.dto;
+
+
+import com.kylych.notesapp.dto.NoteDTO;
+import com.kylych.notesapp.entity.Note;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface NoteMapper {
+
+    NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Note noteDTOToNote(NoteDTO noteDTO);
+
+    NoteDTO noteToNoteDTO(Note note);
+}
---
+++ src/test/java/com/kylych/NoteControllerTest.java
@@ -0,0 +1,2 @@
+package com.kylych;public class NoteControllerTest {
+}
